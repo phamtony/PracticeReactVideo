80aaebd9467b491db385d9447d5c23770ada21d7
Tony Pham <tony.p@lashowroom.com>
2020-12-07 23:51:19 -0800

    Controlled vs Uncontrolled. Making sure data is in state not DOM.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: 'hello there'};
+
+    onInputChange = () => {
+
+    };
+
     render() {
         return (
           <div className="search-bar ui segment">
               <form action="" className="ui form">
                   <div className="field">
                       <label htmlFor="">Video Search</label>
-                      <input type="text"/>
+                      <input
+                          type="text"
+                          value={this.state.term}
+                          onChange={this.onInputChange}
+                      />
                   </div>
               </form>
           </div>
