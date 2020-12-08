6b907b0954c6dc1ab4bb89b8c4894945cdb3b124
Tony Pham <tony.p@lashowroom.com>
2020-12-08 12:06:35 -0800

    youtube api setup and youtubejs setup

---
+++ src/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAs48Caon5nk2WS5e0UMcyI_KvpLcpZOFQ';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
