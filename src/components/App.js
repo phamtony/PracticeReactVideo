2f585306db9cba4d9c1687b0ddbbda6685d75a13
Tony Pham <tony.p@lashowroom.com>
2020-12-08 12:20:04 -0800

    Make the ontermsubmit async. add await on component. assign response to variable response. Once able to make a request to the youtube api, make sure to set the response to state

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import SearchBar from "./SearchBar";
 import youtube from '../api/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({videos: response.data.items});
     };
 
     render() {
