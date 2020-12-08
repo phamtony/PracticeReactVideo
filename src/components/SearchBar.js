1c348c23e414ba1ce0bc6dee2cfebfe7e702b6c4
Tony Pham <tony.p@lashowroom.com>
2020-12-07 23:46:56 -0800

    adding styles -semantic ui + setting up search barC

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+          <div className="search-bar ui segment">
+              <form action="" className="ui form">
+                  <div className="field">
+                      <label htmlFor="">Video Search</label>
+                      <input type="text"/>
+                  </div>
+              </form>
+          </div>
+        );
+    }
+}
+
+export default SearchBar;
